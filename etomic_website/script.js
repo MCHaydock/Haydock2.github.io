8e76675ab79c84f62c6bcfd1678d237618ee8679
MCHaydock <haydockmc97@gmail.com>
2024-12-24 14:41:35 +0000

    Navbar improvements

--- etomic_website/script.js
+++ etomic_website/script.js
@@ -1,20 +1,30 @@
 // JavaScript source code
 let lastScrollY = 0;
 const navbar = document.querySelector('.navbar');
+const logocontainer = document.querySelector('.logo-container');
 
 window.addEventListener('scroll', () => {
     const currentScrollY = window.scrollY;
 
-    if (currentScrollY === 0) {
-        navbar.classList.add('transparent'); //Transparent
-    } else if (!navbar.classList.contains('hidden')) {
-        navbar.classList.remove('transparent');
-    }
-
-    if (currentScrollY > lastScrollY && currentScrollY > 0) {
+    if (currentScrollY > lastScrollY) {
         navbar.classList.add('hidden');
     } else {
         navbar.classList.remove('hidden');
+        console.log("Navbar classes:", navbar.classList);
+    }
+
+    if (!navbar.classList.contains('hidden')) {
+        if (currentScrollY > 0) {
+            navbar.classList.remove('transparent'); //Transparent
+        } else {
+            navbar.classList.add('transparent');
+        }
+    }
+
+    if (currentScrollY > 0) {
+        logocontainer.classList.add('hidden');
+    } else {
+        logocontainer.classList.remove('hidden');
     }
 
     lastScrollY = currentScrollY;
